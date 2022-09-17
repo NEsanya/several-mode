dc453964d5e8a22c5b26fa4d28b0d9133bef0999
remimimimimi <remimimimimi@protonmail.com>
2022-09-17 18:39:38 +0300

    Update version to 0.1.0

--- several-mode.el
+++ several-mode.el
@@ -6,7 +6,7 @@
 ;; Maintainer: remimimimimi <remimimimimi@protonmail.com>
 ;; Created: september 17, 2022
 ;; Modified: september 17, 2022
-;; Version: 0.0.1
+;; Version: 0.1.0
 ;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
 ;; Homepage: https://github.com/remi/several-mode
 ;; Package-Requires: ((emacs "24.3"))
