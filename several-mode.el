3bcd088f25d5747cb2539b283b2ebd86efb4e8a2
remimimimimi <remimimimimi@protonmail.com>
2022-09-17 18:37:47 +0300

    Initialize project elisp file

---
+++ several-mode.el
@@ -0,0 +1,25 @@
+;;; several-mode.el --- View on language syntax from different angle -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 remimimimimi
+;;
+;; Author: remimimimimi <remimimimimi@protonmail.com>
+;; Maintainer: remimimimimi <remimimimimi@protonmail.com>
+;; Created: сентября 17, 2022
+;; Modified: сентября 17, 2022
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/remi/several-mode
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;; This project is an implementation of paper on language syntax theory.
+;;
+;;
+;;; Code:
+
+
+
+(provide 'several-mode)
+;;; several-mode.el ends here
